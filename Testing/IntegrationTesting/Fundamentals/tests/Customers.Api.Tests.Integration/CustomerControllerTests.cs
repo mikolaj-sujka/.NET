05ac2df65b10cb42fa27d24f6ccb054639715c9f
Mikołaj Sujka <mikolajsujka@gmail.com>
2026-01-05 11:36:50 +0100

    learning integration tests

---
+++ Testing/IntegrationTesting/Fundamentals/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Customers.Api.Tests.Integration
+{
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:5001")
+            };
+
+            var response = await httpClient.GetAsync($"/customers/{Guid.NewGuid()}");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
