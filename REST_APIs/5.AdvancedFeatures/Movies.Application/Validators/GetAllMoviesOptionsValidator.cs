6590138b9aef37bb61dba35aa1c8d301640427eb
Mikołaj Sujka <mikolajsujka@gmail.com>
2026-01-14 19:16:41 +0100

    learning some concepcts of rest apis

---
+++ REST_APIs/5.AdvancedFeatures/Movies.Application/Validators/GetAllMoviesOptionsValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Movies.Application.Models;
+
+namespace Movies.Application.Validators;
+
+public class GetAllMoviesOptionsValidator : AbstractValidator<GetAllMoviesOptions>
+{
+    private static readonly string[] AllowedSortByValues =
+    {
+        "title",
+        "yearofrelease"
+    };
+    public GetAllMoviesOptionsValidator()
+    {
+        RuleFor(x => x.YearOfRelease)
+            .LessThanOrEqualTo(DateTime.UtcNow.Year);
+
+        RuleFor(x => x.SortField)
+            .Must(sortField => string.IsNullOrWhiteSpace(sortField) ||
+                               AllowedSortByValues.Contains(sortField.ToLower()))
+            .WithMessage($"SortBy must be one of the following values: {string.Join(", ", AllowedSortByValues)}");
+    }
+}
---
+++ REST_APIs/5.AdvancedFeatures/Movies.Contracts/Requests/GetAllMoviesRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Movies.Contracts.Requests;
+
+public class GetAllMoviesRequest
+{
+    public required string? Title { get; set; }
+    public required int? Year { get; set; }
+    public required string? SortBy { get; set; }
+}
