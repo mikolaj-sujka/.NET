42abedae44d15ff201bfba10950479478a451e5c
Mikołaj Sujka <mikolajsujka@gmail.com>
2026-01-19 20:17:17 +0100

    add razor pages

--- Auth/Marvin.IDP/Config.cs
+++ Auth/Marvin.IDP/Config.cs
@@ -5,16 +5,14 @@ namespace Marvin.IDP;
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
-        };
+    [
+        new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+    ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-            { };
+        [];
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        [];
 }
---
+++ Auth/Marvin.IDP/Pages/TestUsers.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityModel;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Test;
+
+namespace Marvin.IDP;
+
+public static class TestUsers
+{
+    public static List<TestUser> Users
+    {
+        get
+        {
+            var address = new
+            {
+                street_address = "One Hacker Way",
+                locality = "Heidelberg",
+                postal_code = "69118",
+                country = "Germany"
+            };
+                
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "123309e9-03aa-4b63-b367-59f1e03520a4",
+                    Username = "Alice",
+                    Password = "password",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "Alice"),
+                        new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "41ecc841-2005-4cda-bd20-6c51b761042d",
+                    Username = "Bob",
+                    Password = "password",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "Bob"),
+                        new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    }
+                }
+            };
+        }
+    }
+}
