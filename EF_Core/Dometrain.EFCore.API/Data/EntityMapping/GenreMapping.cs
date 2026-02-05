b282bd315a7c3dbb38cd63408cd257700feb824e
Mikołaj Sujka <mikolajsujka@gmail.com>
2026-02-05 20:26:14 +0100

    add some tests.

---
+++ EF_Core/Dometrain.EFCore.API/Data/EntityMapping/GenreMapping.cs
@@ -0,0 +1,16 @@
+using Dometrain.EFCore.API.Data.ValueGenerators;
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dometrain.EFCore.API.Data.EntityMapping;
+
+public class GenreMapping : IEntityTypeConfiguration<Genre>
+{
+    public void Configure(EntityTypeBuilder<Genre> builder)
+    {
+        builder.Property<DateTime>("CreatedDate")
+            .HasColumnName("CreatedAt")
+            .HasValueGenerator<CreatedDateGenerator>();
+    }
+}
---
+++ EF_Core/Dometrain.EFCore.API/Data/EntityMapping/MovieMapping.cs
@@ -0,0 +1,44 @@
+using Dometrain.EFCore.API.Data.ValueConverters;
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Dometrain.EFCore.API.Data.EntityMapping;
+
+public class MovieMapping : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder
+            .ToTable("Pictures")
+            .HasQueryFilter(movie => movie.ReleaseDate > new DateTime(1990,1,1))
+            .HasKey(movie => movie.Id);
+
+        builder.Property(movie => movie.Title)
+            .HasColumnType("varchar")
+            .HasMaxLength(128)
+            .IsRequired();
+
+        builder.Property(movie => movie.ReleaseDate)
+            .HasColumnType("char(8)")
+            .HasConversion(new DateTimeToChar8Converter());
+
+        builder.Property(movie => movie.Synopsis)
+            .HasColumnType("varchar(max)")
+            .HasColumnName("Plot");
+
+        builder
+            .HasOne(movie => movie.Genre)
+            .WithMany(genre => genre.Movies)
+            .HasPrincipalKey(genre => genre.Id)
+            .HasForeignKey(movie => movie.MainGenreId);
+
+        builder
+            .OwnsOne(movie => movie.Director)
+            .ToTable("Pictures_Directors"); 
+        
+        builder
+            .OwnsMany(movie => movie.Actors);
+    }
+}
